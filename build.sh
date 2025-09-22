e9f8fd90ec0011937df49648b3688d768459b270
Matteo Piscitelli <matteo@matteopiscitelli.it>
2025-09-22 16:03:14 +0200

    First implementation.

---
+++ build.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+# exit on error
+set -e
+
+git clone --single-branch --depth 50 https://github.com/indilib/indi-3rdparty.git
+pushd indi-3rdparty
+git checkout v2.1.5
+
+BUILD=../
+
+LIBS=$( ls -d lib* )
+DRVS=$( ls -d indi* )
+
+mkdir -p $BUILD
+
+for lib in $LIBS; do
+    [ -d $BUILD/deb-$lib ] && rm -rf $BUILD/deb-$lib
+    mkdir $BUILD/deb-$lib
+    cp -r $lib $BUILD/deb-$lib
+    cp -r debian/$lib $BUILD/deb-$lib/debian
+    cp -r cmake_modules $BUILD/deb-$lib/
+    pushd $BUILD/deb-$lib
+    #dpkg-buildpackage -uc -us -b || { echo "Failed building $lib."; exit 1; }
+    chmod 755 debian/rules
+    fakeroot debian/rules binary
+    popd
+    pushd $BUILD
+    sudo dpkg -i ${lib}_*.deb
+    popd
+done
+
+for drv in $DRVS; do
+    [ -d $BUILD/deb-$drv ] && rm -rf $BUILD/deb-$drv
+    mkdir $BUILD/deb-$drv
+    cp -r $drv $BUILD/deb-$drv
+    cp -r debian/$drv $BUILD/deb-$drv/debian
+    cp -r cmake_modules $BUILD/deb-$drv/
+    pushd $BUILD/deb-$drv
+    #dpkg-buildpackage -uc -us -b || { echo "Failed building $lib."; exit 1; }
+    chmod 755 debian/rules
+    fakeroot debian/rules binary
+    popd
+done
+
+popd #indi-3rdparty
+
+# move packages outside
+mv *.dev ../
+
