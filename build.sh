9637ba3eb188d0be0f860150dbc9e793c759123c
Matteo Piscitelli <matteo@matteopiscitelli.it>
2025-10-03 10:37:51 +0200

    Reimplement build with static pkg list to deal with cross deps. Close #4.
    Some package is cross dependent from another one: a static list
    is useful to force the build order so that dependent ones are built
    after their dependency.

--- build.sh
+++ build.sh
@@ -3,11 +3,10 @@
 # exit on error
 set -e
 
-BLACKLIST=
-
-[ -f blacklist.lst ] && BLACKLIST=$( cat blacklist.lst )
-
 ./download.sh v2.1.5
+
+# get packages list
+PKGLIST=$( cat pkgs.conf | egrep -v '^#' )
 
 # enter source folder
 pushd indi-3rdparty
@@ -15,27 +14,11 @@ pushd indi-3rdparty
 # Call script to apply patches
 ../apply-patches.sh
 
-# get packages lists
-LIBS=$( ls -d lib* )
-DRVS=$( ls -d indi* )
-
-# build and install libs
-for lib in $LIBS; do
-    for b in $BLACKLIST; do
-        [ "$b" = "$lib" ] && continue 2 #skip blacklisted lib
-    done
-
-    # build package (and install it since it's a library)
-    ../build-package.sh $lib install
-done
-
-for drv in $DRVS; do
-    for b in $BLACKLIST; do
-        [ "$b" = "$drv" ] && continue 2 #skip blacklisted drv
-    done
-
-    # build package
-    ../build-package.sh $drv
+# build (and install if libs)
+for PKG in $PKGLIST; do
+    TOINST=
+    [ "${PKG::3}" = "lib" ] && TOINST=install
+    ../build-package.sh $PKG $TOINST
 done
 
 popd #indi-3rdparty
